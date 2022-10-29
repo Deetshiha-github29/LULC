aa98105ed9850f2d3a010c2a5f9b5ec51ceff019
JOTHI DEETSHIHA D <87648060+Deetshiha-github29@users.noreply.github.com>
2022-10-29 10:33:04 +0530

    Create plot_rgb.py

---
+++ plot_rgb.py
@@ -0,0 +1,10 @@
+ep.plot_rgb(
+    arr_st,
+    rgb=(3, 2, 1),
+    stretch=True,
+    str_clip=0.02,
+    figsize=(12, 16),
+    # title="RGB Composite Image with Stretch Applied",
+)
+
+plt.show()
